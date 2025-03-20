3ef40c35e29d16815a9aebe0d4d15adf7b3666fb
Mårten Svantesson <Marten.Svantesson@ticket.se>
2025-03-20 21:52:28 +0100

    fix: ignore stdout
    helmfile might print warnings to stderr that we want to ingore at them moment

--- pkg/helmfiles/list.go
+++ pkg/helmfiles/list.go
@@ -1,6 +1,7 @@
 package helmfiles
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"strings"
@@ -19,20 +20,22 @@ type HelmRelease struct {
 // ListReleases lists the releases in the helmfile
 func ListReleases(runner cmdrunner.CommandRunner, file string, env map[string]string) ([]HelmRelease, error) {
 	args := []string{"--file", file, "list", "--output", "json"}
+	var b bytes.Buffer
 	c := &cmdrunner.Command{
 		Name: "helmfile",
 		Args: args,
 		Env:  env,
+		Out:  &b,
 	}
-	output, err := runner(c)
+	_, err := runner(c)
 	if err != nil {
 		return nil, fmt.Errorf("failed to run %s: %w", c.CLI(), err)
 	}
-	output = strings.TrimSpace(output)
+	output := strings.TrimSpace(b.String())
 	if output == "" {
 		return nil, nil
 	}
-	answer := []HelmRelease{}
+	var answer []HelmRelease
 
 	err = json.Unmarshal([]byte(output), &answer)
 	if err != nil {
