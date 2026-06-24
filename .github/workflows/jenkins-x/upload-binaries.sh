8e7343d947dd64221f3db0f87a0673a0673e7c6f
GavinPJK <gavin.kirwan@mqube.com>
2026-06-24 10:07:17 +0100

    chore: bump syft version

--- .github/workflows/jenkins-x/upload-binaries.sh
+++ .github/workflows/jenkins-x/upload-binaries.sh
@@ -27,7 +27,7 @@ export ROOTPACKAGE="github.com/$REPOSITORY"
 
 # Install syft in this script
 curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | \
-sh -s -- -b /usr/local/bin v0.54.0
+sh -s -- -b /usr/local/bin v1.43.0
 chmod +x /usr/local/bin/syft
 
 goreleaser release
